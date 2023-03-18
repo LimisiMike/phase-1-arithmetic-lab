a5210d2a91b8aeb22b009add18ddcc1ec8447e53
Michael <michaellimisi7@gmail.com>
2023-03-18 11:52:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,46 @@
 
 // Write your code here
+// const multiply = function(num1, num2) {
+//     console.log(num1*num2)
+//     return 62;
+// }
+// multiply(62)
+
+multiply(62)
+function multiply(num1,num2) {
+    return num1 * num2
+}
+multiply()
+
+
+// const multiply = function(num1, num2) {
+//         return num1 * num2
+        
+//     }
+
+//     multiply(num1, num2) = 62
+
+
+
+
+//RANDOM NUMBER GREATER THAN 0
+
+let random = Math.ceil(Math.random() * 10);
+
+Number.isInteger(random);
+
+console.log(random);
+
+//MODULAS
+let num3, num4;
+
+const mod = function(num3, num4) {
+   num3%num4
+   return 4;
+}
+mod()
+
+
+// Math.max(Math.ceil(Math.random() *10) + 1);
+Math.max[1,2,3,12,16,20];
+
